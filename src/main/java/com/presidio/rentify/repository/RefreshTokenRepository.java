5b1e163d7ed99cab136627d07dbb7ec7535c42ed
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-06-06 23:51:03 +0530

    added findByUser method in RefreshTokenRepository

--- src/main/java/com/presidio/rentify/repository/RefreshTokenRepository.java
+++ src/main/java/com/presidio/rentify/repository/RefreshTokenRepository.java
@@ -1,13 +1,17 @@
 package com.presidio.rentify.repository;
 
 import com.presidio.rentify.entity.RefreshToken;
+import com.presidio.rentify.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Ref;
 import java.util.Optional;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
+
+    Optional<RefreshToken> findByUser(User user);
 
     void deleteByRefreshToken(String refreshToken);
 }
